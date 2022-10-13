b093d9f7b4e6611763e0694419f7bdca2f017b6b
Philip Dimaano <pdimaano7@gmail.com>
2022-10-13 16:44:44 -0700

    Setup questions with radio buttons, working on redirects

--- app.py
+++ app.py
@@ -10,17 +10,50 @@ debug = DebugToolbarExtension(app)
 
 responses = []
 
+
 @app.get("/")
 def build_landing_page():
     title = survey.title
     instructions = survey.instructions
     return render_template("survey_start.html", title=title,
-        instructions=instructions)
+                           instructions=instructions)
 
-# @app.post("/question/<question_Id>")
-# def show_current_question(question_Id):
-#     question_instance = survey.questions[question_Id]
 
-#     return render_template("question.html", question = question_instance)
+@app.post("/begin")
+def make_zero_page():
 
-#@app.post("/begin")
+    return render_template("question.html", )
+
+
+@app.get("/question/<question_Id>")
+def show_current_question(question_Id):
+    question_instance = survey.questions[int(question_Id)]
+    breakpoint()
+    print(question_instance)
+    return render_template("question.html", question=question_instance)
+
+
+@app.post("/answer")
+def get_answer_choices():
+    responses.append(request.form["answer"])
+    print(responses)
+
+    return redirect("/question/1")
+
+
+# @app.post("/post-example")
+# def post_example():
+#     """An example of good POST handling."""
+
+#     isbn = request.form["isbn"]
+
+#     if isbn in VALID_ISBNS:
+#         print(f"\n\nBuying Book: {isbn}\n\n")
+
+#         # flash message: we'll talk about this soon
+#         # flash(f"Book {isbn} bought!")
+
+#         return redirect("/thanks")
+#     else:
+#         error = "Invalid isbn"
+#         return render_template("post-form.html", isbn=isbn, error=error)
