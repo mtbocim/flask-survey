de9402842f927c68ffb8085704345d7765b8d9cc
michael-bocim <mtbocim@gmail.com>
2022-10-13 18:35:15 -0600

    up through step 5

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from surveys import satisfaction_survey as survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "never-tell!"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -21,39 +21,28 @@ def build_landing_page():
 
 @app.post("/begin")
 def make_zero_page():
-
-    return render_template("question.html", )
+    responses.clear()
+    return redirect("/question/0")
 
 
 @app.get("/question/<question_Id>")
 def show_current_question(question_Id):
     question_instance = survey.questions[int(question_Id)]
-    breakpoint()
+    #breakpoint()
     print(question_instance)
+    
     return render_template("question.html", question=question_instance)
 
 
 @app.post("/answer")
 def get_answer_choices():
     responses.append(request.form["answer"])
-    print(responses)
-
-    return redirect("/question/1")
-
-
-# @app.post("/post-example")
-# def post_example():
-#     """An example of good POST handling."""
-
-#     isbn = request.form["isbn"]
-
-#     if isbn in VALID_ISBNS:
-#         print(f"\n\nBuying Book: {isbn}\n\n")
-
-#         # flash message: we'll talk about this soon
-#         # flash(f"Book {isbn} bought!")
-
-#         return redirect("/thanks")
-#     else:
-#         error = "Invalid isbn"
-#         return render_template("post-form.html", isbn=isbn, error=error)
+    if len(responses)<len(survey.questions):
+        next_question = len(responses)
+        return redirect(f"/question/{next_question}")
+    else:
+        completed_responses = []
+        for i in enumerate(survey.questions):
+            completed_responses.append([i[1], responses[i[0]]])
+        return render_template("completion.html", 
+            responses=completed_responses)
