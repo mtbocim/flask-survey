507d82e361ea7ceb3c6909d5ecd5850a96f9c53f
michael-bocim <mtbocim@gmail.com>
2022-10-13 18:58:24 -0600

    Added doc strings and formatted stuff

--- app.py
+++ app.py
@@ -13,36 +13,52 @@ responses = []
 
 @app.get("/")
 def build_landing_page():
+    """Render start page with title and instructions"""
+
     title = survey.title
     instructions = survey.instructions
-    return render_template("survey_start.html", title=title,
-                           instructions=instructions)
+    return render_template(
+        "survey_start.html",
+        title=title,
+        instructions=instructions
+    )
 
 
 @app.post("/begin")
 def make_zero_page():
+    """Clears response list and redirects to first question"""
+
     responses.clear()
     return redirect("/question/0")
 
 
-@app.get("/question/<question_Id>")
+@app.get("/question/<int:question_Id>")
 def show_current_question(question_Id):
-    question_instance = survey.questions[int(question_Id)]
-    #breakpoint()
-    print(question_instance)
-    
-    return render_template("question.html", question=question_instance)
+    """Renders and displays current questions"""
+
+    question_instance = survey.questions[question_Id]
+    # breakpoint()
+    return render_template(
+        "question.html",
+        question=question_instance
+    )
 
 
 @app.post("/answer")
 def get_answer_choices():
+    """Appends answer to response and either
+    continues to new question or shows results
+    of all questions"""
+
     responses.append(request.form["answer"])
-    if len(responses)<len(survey.questions):
+    if len(responses) < len(survey.questions):
         next_question = len(responses)
         return redirect(f"/question/{next_question}")
     else:
         completed_responses = []
-        for i in enumerate(survey.questions):
-            completed_responses.append([i[1], responses[i[0]]])
-        return render_template("completion.html", 
-            responses=completed_responses)
+        for i, question in enumerate(survey.questions):
+            completed_responses.append([question, responses[i]])
+        return render_template(
+            "completion.html",
+            responses=completed_responses
+        )
